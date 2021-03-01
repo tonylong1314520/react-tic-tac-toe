04da75364e47f023b57a792e32b9efdb7937b828
Tony Long <tonylong@hawaii.edu>
2021-02-28 23:36:50 -1000

    Task 3

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-        <button className="square">
-          {/* TODO */}
+        <button
+            className="square"
+            onClick={() => this.setState({value: 'X'})}
+        >
+          {this.state.value}
         </button>
     );
   }
